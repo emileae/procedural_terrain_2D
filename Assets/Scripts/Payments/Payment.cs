94d7949c0a62fb0c125cfb47030de6b31a45431f
Emile <emile.app@gmail.com>
2017-03-10 13:14:51 +0200

    added currency indicators

--- Assets/Scripts/Payments/Payment.cs
+++ Assets/Scripts/Payments/Payment.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor.Animations;
 
 public class Payment : MonoBehaviour {
 
@@ -10,6 +11,8 @@ public class Payment : MonoBehaviour {
 	public int useCost = 1;
 	private int amountPaid = 0;
 	private bool paid = false;
+	public GameObject fishOutline;
+	public GameObject currencyIndicatorContainer;
 
 	public bool Pay ()
 	{
@@ -35,10 +38,39 @@ public class Payment : MonoBehaviour {
 		return paid;
 	}
 
+	void ShowCost ()
+	{
+		Bounds bounds = gameObject.transform.GetChild (0).GetComponent<MeshRenderer> ().bounds;
+		int cost;
+		if (level == 0) {
+			Debug.Log ("Cost = " + buildCost);
+			cost = buildCost;
+		} else {
+			Debug.Log ("Cost = " + useCost);
+			cost = useCost;
+		}
+
+		for (int i = 0; i < cost; i++) {
+			GameObject currencyFishClone = Instantiate(fishOutline, new Vector3(transform.position.x, transform.position.y + (1.5f * bounds.size.y) + (i * 5), transform.position.z), Quaternion.Euler(-90, 0, 0)) as GameObject;
+			currencyFishClone.transform.parent = currencyIndicatorContainer.transform;
+		}
+
+	}
+
+	void HideCost ()
+	{
+		foreach (Transform child in currencyIndicatorContainer.transform) {
+			Destroy(child.gameObject);
+		}
+	}
+
 	void OnTriggerEnter2D (Collider2D col)
 	{
 		GameObject go = col.gameObject;
 		if (go.tag == "Player") {
+
+			ShowCost();
+
 			PlayerController playerScript = go.GetComponent<PlayerController>();
 			playerScript.payTarget = gameObject;
 		}
@@ -48,6 +80,9 @@ public class Payment : MonoBehaviour {
 	{
 		GameObject go = col.gameObject;
 		if (go.tag == "Player") {
+
+			HideCost();
+
 			PlayerController playerScript = go.GetComponent<PlayerController>();
 			playerScript.payTarget = null;
 			// if player has paid something but exits before completing payment
